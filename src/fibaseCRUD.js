8694a8dd60120cf5d8d83e0a37bc790c056d3570
Fedorchuk-Valeria <74813522+Fedorchuk-Valeria@users.noreply.github.com>
2023-05-09 15:31:22 +0300

    Add files via upload

---
+++ src/fibaseCRUD.js
@@ -0,0 +1,31 @@
+export function getUsers(thenCallback) {
+    get(child(dbref, "users")).then( data => {
+        if(data.exists()){
+            thenCallback(data.val())
+        }
+    })
+}
+
+export function getLessons(thenCallback) {
+    get(child(dbref, "lessons/")).then( data => {
+        if(data.exists()){
+            thenCallback(data.val())
+        }
+    })
+}
+
+export function getModules(thenCallback) {
+    get(child(dbref, "modules/")).then( data => {
+        if(data.exists()){
+            thenCallback(data.val())
+        }
+    })
+}
+
+export function getAges(thenCallback) {
+    get(child(dbref, "ages/")).then( data => {
+        if(data.exists()){
+            thenCallback(data.val())
+        }
+    })
+}
