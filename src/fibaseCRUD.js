219faea048f76b455f20d1b5bc088285e5658617
Fedorchuk-Valeria <74813522+Fedorchuk-Valeria@users.noreply.github.com>
2023-05-09 19:32:26 +0300

    Add files via upload

--- src/fibaseCRUD.js
+++ src/fibaseCRUD.js
@@ -1,5 +1,3 @@
-import {database, dbref, app, getDatabase, set, get, update, remove, push, ref, query, limitToLast, child, onValue} from './firebaseInitializer.js'
-
 export function getUsers(thenCallback) {
     return get(child(dbref, "users")).then( data => {
         if(data.exists()){
@@ -8,10 +6,18 @@ export function getUsers(thenCallback) {
     })
 }
 
-export function getLessons(thenCallback) {
+export function getLessons (thenCallback) {
     return get(child(dbref, "lessons/")).then( data => {
         if(data.exists()){
             thenCallback(data.val())
+        }
+    })
+}
+
+export function getLastLesson (thenCallback) {
+    return get(query(ref(database, 'lessons/'), limitToLast(1))).then(data=> {
+        if(data.exists()){
+            thenCallback()
         }
     })
 }
@@ -25,7 +31,7 @@ export function getModules(thenCallback) {
 }
 
 export function getModuleById(id, thenCallback) {
-    get(child(dbref, "modules/" + id)).then( data => {
+    return get(child(dbref, "modules/" + id)).then( data => {
         if(data.exists()){
             thenCallback(data.val())
         }
@@ -38,4 +44,39 @@ export function getAges(thenCallback) {
             thenCallback(data.val())
         }
     })
+}
+
+export function setLesson(id, lesson, userId) {
+    return set(ref(database, "lessons/" + id), {
+        age: lesson.age,
+        startModule: lesson.startModule,
+        startTheme: lesson.startTheme,
+        startDate: lesson.startDate,
+        lessonType: lesson.lessonType,
+        userId: userId
+    }).then(() => console.log("ok"))
+    .catch(error => console.log(error))
+}
+
+export function setModule(id, module) {
+    return set(ref(database, "modules/" + id), {
+        name: module.name,
+        themes: module.themes
+    }).then(() => {console.log("ok"); UpdateModules()})
+    .catch(error => console.log(error))
+}
+
+export function removeModule(id) {
+    return remove(ref(database, "modules/" + id))
+}
+
+export function updateLesson(id, lesson, thenCallback) {
+    return update(ref(database, "lessons/" + id), {
+        startDate: lesson.startDate,
+        startModule: lesson.startModule,
+        startTheme: lesson.startTheme,
+    }).then(() => {
+        thenCallback()
+    })
+    .catch(error => console.log(error))
 }
