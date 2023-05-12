1e2e60e758b663574a093b99f132ecde7b634c76
Fedorchuk-Valeria <74813522+Fedorchuk-Valeria@users.noreply.github.com>
2023-05-12 19:18:15 +0300

    Update Timetable.js

--- src/Timetable.js
+++ src/Timetable.js
@@ -4,73 +4,72 @@ import {UpdateLessons} from './TimetableLessonFiltration.js'
 
 let dbref = ref(database)
 
-      //_________________________________________________________________________________________
-      let lessons = []
-      let newLessons = []
-      //_________________________________________________________________________________________
-      function reload() {
-      UpdateLessons().then(lessons => {
-      const getLessonHTML = (lesson) => 
-              `<li class="space-between indented">` +
-              `  <div class="space-evenly">` +
-              `    <p>${lesson.age}</p>` +
-              `    <p>${lesson.lessonType}</p>` +
-              `    <p>${lesson.startModule}</p>` +
-              `    <p>${lesson.startTheme}</p>` +
-              `  </div>` +
-              
-              `  <div class="row">` +
-              `    <button class="no_styles_button peach_background_color editLessonButton" id="${lesson.id}">Edit</button>` +
-              `    <button class="no_styles_button peach_background_color deleteLessonButton" id="${lesson.id}">Delete</button>` +
-              `  </div>` +
-              `</li>`
+//_________________________________________________________________________________________
+let lessons = []
+let newLessons = []
+//_________________________________________________________________________________________
+function reload() {
+
+    UpdateLessons().then(lessons => {
+        const getLessonHTML = (lesson) => 
+            `<li class="space-between indented">` +
+            `  <div class="space-evenly">` +
+            `    <p>${lesson.age}</p>` +
+            `    <p>${lesson.lessonType}</p>` +
+            `    <p>${lesson.startModule}</p>` +
+            `    <p>${lesson.startTheme}</p>` +
+            `  </div>` +
+            
+            `  <div class="row">` +
+            `    <button class="no_styles_button peach_background_color editLessonButton" id="${lesson.id}">Edit</button>` +
+            `    <button class="no_styles_button peach_background_color deleteLessonButton" id="${lesson.id}">Delete</button>` +
+            `  </div>` +
+            `</li>`
               
               // console.log(mondayLessons)
               // console.log(mondayLessons.innerHTML)
               
-              const mondayLessons = document.getElementById("mondayLessons")
-              const tuesdayLessons = document.getElementById("tuesdayLessons")
-              const wednesdayLessons = document.getElementById("wednesdayLessons")
-              const thursdayLessons = document.getElementById("thursdayLessons")
-              const fridayLessons = document.getElementById("fridayLessons")
-              const saturdayLessons = document.getElementById("saturdayLessons")
-              const sundayLessons = document.getElementById("sundayLessons")
+        const mondayLessons = document.getElementById("mondayLessons")
+        const tuesdayLessons = document.getElementById("tuesdayLessons")
+        const wednesdayLessons = document.getElementById("wednesdayLessons")
+        const thursdayLessons = document.getElementById("thursdayLessons")
+        const fridayLessons = document.getElementById("fridayLessons")
+        const saturdayLessons = document.getElementById("saturdayLessons")
+        const sundayLessons = document.getElementById("sundayLessons")
               
-              newLessons.forEach(lesson => {
-                  const lessonStartDate = new Date(lesson.startDate)
+        newLessons.forEach(lesson => {
+            const lessonStartDate = new Date(lesson.startDate)
 
-                  if(lessonStartDate.getDay() === 0){ // sunday
-                    sundayLessons.innerHTML += getLessonHTML(lesson)
-                }
-                else if(lessonStartDate.getDay() === 1){ // monday
-                    mondayLessons.innerHTML += getLessonHTML(lesson)
-                }
-                else if(lessonStartDate.getDay() === 2){ // tuesday
-                  tuesdayLessons.innerHTML += getLessonHTML(lesson)
-                }
-                else if(lessonStartDate.getDay() === 3){ // wednes
-                    wednesdayLessons.innerHTML += getLessonHTML(lesson)
-                }
-                else if(lessonStartDate.getDay() === 4){ // thursday
-                    thursdayLessons.innerHTML += getLessonHTML(lesson)
-                }
-                else if(lessonStartDate.getDay() === 5){ // friday
-                  fridayLessons.innerHTML += getLessonHTML(lesson)
-                }
-                else if(lessonStartDate.getDay() === 6){ // saturday
-                  saturdayLessons.innerHTML += getLessonHTML(lesson)
-                }
-              })
-            })
-          
-        }).then(()=> {
-          let deleteLessonButtons = document.querySelectorAll(".deleteLessonButton");
+            if(lessonStartDate.getDay() === 0){ // sunday
+                sundayLessons.innerHTML += getLessonHTML(lesson)
+            }
+            else if(lessonStartDate.getDay() === 1){ // monday
+                mondayLessons.innerHTML += getLessonHTML(lesson)
+            }
+            else if(lessonStartDate.getDay() === 2){ // tuesday
+                tuesdayLessons.innerHTML += getLessonHTML(lesson)
+            }
+            else if(lessonStartDate.getDay() === 3){ // wednes
+                wednesdayLessons.innerHTML += getLessonHTML(lesson)
+            }
+            else if(lessonStartDate.getDay() === 4){ // thursday
+                thursdayLessons.innerHTML += getLessonHTML(lesson)
+            }
+            else if(lessonStartDate.getDay() === 5){ // friday
+              fridayLessons.innerHTML += getLessonHTML(lesson)
+            }
+            else if(lessonStartDate.getDay() === 6){ // saturday
+              saturdayLessons.innerHTML += getLessonHTML(lesson)
+            }
+        })
+    }).then(()=> {
+        let deleteLessonButtons = document.querySelectorAll(".deleteLessonButton");
 
-          deleteLessonButtons.forEach(button=> {
-            button.addEventListener('click', e => {
-              e.preventDefault()
-              const id = button.id
-              remove(ref(database, "lessons/" + id.toString()))
+        deleteLessonButtons.forEach(button=> {
+        button.addEventListener('click', e => {
+            e.preventDefault()
+            const id = button.id
+            remove(ref(database, "lessons/" + id.toString()))
               sundayLessons.innerHTML = " "
               mondayLessons.innerHTML = " "
               tuesdayLessons.innerHTML = " "
@@ -80,26 +79,23 @@ let dbref = ref(database)
               saturdayLessons.innerHTML = " "
               reload()
             })
-          })
+        })
           
-          let editLessonButtons = document.querySelectorAll(".editLessonButton");
+        let editLessonButtons = document.querySelectorAll(".editLessonButton");
 
-          editLessonButtons.forEach(button=> {
-              button.addEventListener('click', e => {
-                  e.preventDefault()
-                  const id = button.id
-                  
-                  sessionStorage.setItem('currLesson', id)
-                  let currlesson = newLessons.filter(l => l.id.toString() === id.toString())[0]
-                  console.log(currlesson)
-                  
-                  sessionStorage.setItem('currLesson', JSON.stringify(currlesson))
-                  
-              window.location.href = './UpdateLesson.html';
-              
+        editLessonButtons.forEach(button=> {
+            button.addEventListener('click', e => {
+                e.preventDefault()
+                const id = button.id
+                
+                sessionStorage.setItem('currLesson', id)
+                let currlesson = newLessons.filter(l => l.id.toString() === id.toString())[0]
+                console.log(currlesson)
+                
+                sessionStorage.setItem('currLesson', JSON.stringify(currlesson))
+                
+                window.location.href = './UpdateLesson.html';
             })
-      })
-  }
-
-      
-
+        })
+    })
+}
